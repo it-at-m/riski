a490b153a63935c1f200a32d26d496e49be62ee2
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-08-01 10:47:20 +0200

    chore: code rabbit suggestions

--- riski-extractor/app.py
+++ riski-extractor/app.py
@@ -6,7 +6,6 @@ from src import extract
 from src.logtools import getLogger
 from src.version import get_version
 
-DEFAULT_START_URL = "https://risi.muenchen.de/risi/sitzung/uebersicht"
 DEFAULT_START_DATE = datetime.date.today().isoformat()
 
 
@@ -15,7 +14,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser(
         description="RIS Indexer - Collect, Extract, Transform, and Load data from Rathausinformationssystem website"
     )
-    parser.add_argument("--starturl", default=DEFAULT_START_URL, help=f"URL to the sitemap (default: {DEFAULT_START_URL})")
     parser.add_argument(
         "--startdate", default=DEFAULT_START_DATE, help="Startdate for filtering STR-Meetings in ISO format (default: today's date)"
     )
@@ -31,7 +29,7 @@ def main():
     extractor = extract.RISExtractor()
     startdate = datetime.date.fromisoformat(args.startdate)
     logger.info(f"Extracting meetings starting from {startdate}")
-    extract_artifacts = extractor.run(args.starturl, startdate)
+    extract_artifacts = extractor.run(startdate)
     print(extract_artifacts)
     logger.info("Extraction process finished")
     logger.info("RIS Indexer completed successfully")
