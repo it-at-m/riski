c5c348af58c899c87dc037ecc1437fb9dea928af
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-08-25 10:22:30 +0200

    refactor: print -> logger

--- riski-extractor/app.py
+++ riski-extractor/app.py
@@ -39,13 +39,13 @@ def main():
     sitzungen_extractor = StadtratssitzungenExtractor()
     extracted_meeting_list = sitzungen_extractor.run(startdate)
     logger.info(f"Extracted {len(extracted_meeting_list)} meetings")
-    print([obj.name for obj in extracted_meeting_list])
+    logger.info([obj.name for obj in extracted_meeting_list])
 
     logger.info("Extracting Heads of Departments")
     head_of_department_extractor = HeadOfDepartmentExtractor()
     extracted_head_of_department_list = head_of_department_extractor.run(startdate)
     logger.info(f"Extracted {len(extracted_head_of_department_list)} Heads of Departments")
-    print([obj.familyName for obj in extracted_head_of_department_list])
+    logger.info([obj.familyName for obj in extracted_head_of_department_list])
 
     logger.info("Extraction process finished")
     logger.info("RIS Indexer completed successfully")
