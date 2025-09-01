f50e335e414246b6f8f130335d06133b764435f6
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-09-01 09:22:01 +0200

    refactor: general methods for person parser

--- riski-extractor/src/parser/city_council_member_parser.py
+++ riski-extractor/src/parser/city_council_member_parser.py
@@ -1,4 +1,3 @@
-import re
 from datetime import datetime, timezone
 from logging import Logger
 
@@ -6,10 +5,10 @@ from bs4 import BeautifulSoup
 
 from src.data_models import Person
 from src.logtools import getLogger
-from src.parser.base_parser import BaseParser
+from src.parser.person_base_parser import PersonBaseParser
 
 
-class CityCouncilMemberParser(BaseParser[Person]):
+class CityCouncilMemberParser(PersonBaseParser):
     """
     Parser for city council member
     """
@@ -19,47 +18,6 @@ class CityCouncilMemberParser(BaseParser[Person]):
     def __init__(self) -> None:
         self.logger = getLogger()
         self.logger.info("Council member parser initialized.")
-
-    def _get_titles(self, titles: list[str]) -> list[str]:
-        """
-        Method for extracting the academic title of a person from their name.
-        A list of potential titles is passed to the method.
-        This list is best obtained by splitting the name at all spaces.
-        Then all substrings with special characters that are not '-' are determined and transferred.
-
-        With these strings, it is possible that a title such as Dr.(Univ. Florence) is split into ['Dr.(Univ.','Florence)']
-        These must be put together again to form a title.
-        All strings are run through in sequence and if a '(' without ')' is found, a temporary
-        string is created and the existing parts are joined together to form a string until a ')' is found.
-        """
-
-        title_array = []
-        current_academic_title = ""
-        in_title = False
-
-        for title_part in titles:
-            if "(" in title_part and ")" not in title_part:
-                current_academic_title += title_part
-                in_title = True
-            elif ")" in title_part and in_title:
-                current_academic_title += " " + title_part
-                in_title = False
-                title_array.append(current_academic_title)
-                current_academic_title = ""
-            elif in_title:
-                current_academic_title += title_part
-            else:
-                title_array.append(title_part)
-
-        return title_array
-
-    def _is_no_name(self, name: str) -> bool:
-        special_char_regex = r"[\.\(\)]"
-        return re.search(special_char_regex, name) is not None
-
-    def _is_form_of_address(self, name: str) -> bool:
-        anrede_regex = r"^(Frau|Herr)$"
-        return re.search(anrede_regex, name) is not None
 
     def parse(self, url: str, html: str) -> Person:
         self.logger.debug(f"Parsing city council member: {url}")
