d0e45397bfe0dee122d8f50b8b450e816e4303aa
a-wittmann <63039773+a-wittmann@users.noreply.github.com>
2025-09-02 15:10:19 +0200

    feat: minimal extractor and parser

--- riski-extractor/app.py
+++ riski-extractor/app.py
@@ -2,6 +2,7 @@ import sys
 from logging import Logger
 
 from config.config import Config, get_config
+from src.extractor.city_council_motion_extractor import CityCouncilMotionExtractor
 from src.extractor.head_of_department_extractor import HeadOfDepartmentExtractor
 from src.extractor.stadtratssitzungen_extractor import StadtratssitzungenExtractor
 from src.logtools import getLogger
@@ -30,6 +31,12 @@ def main():
     extracted_head_of_department_list = head_of_department_extractor.run()
     logger.info(f"Extracted {len(extracted_head_of_department_list)} Heads of Departments")
     logger.info([obj.familyName for obj in extracted_head_of_department_list])
+
+    logger.info("Extracting City Council Motion")
+    city_council_motion_extractor = CityCouncilMotionExtractor()
+    extracted_city_council_motion_list = city_council_motion_extractor.run()
+    logger.info(f"Extracted {len(extracted_city_council_motion_list)} Heads of Departments")
+    logger.info([obj.name for obj in extracted_city_council_motion_list])
 
     logger.info("Extraction process finished")
     logger.info("RIS Indexer completed successfully")
