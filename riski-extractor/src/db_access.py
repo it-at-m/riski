b95c16da40ed8ab91e690a9c734ff99e437f0121
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-09-18 11:35:09 +0200

    fix: long living session

--- riski-extractor/src/db_access.py
+++ riski-extractor/src/db_access.py
@@ -4,6 +4,7 @@ from sqlmodel import Session, SQLModel, create_engine, select
 from src.data_models import Person
 
 _engine = create_engine(str(get_config().database_url), echo=True)
+_session = Session(_engine)
 
 
 def create_tables():
@@ -11,17 +12,17 @@ def create_tables():
 
 
 def request_person_by_risid(risid: str):
-    with Session(_engine) as session:
-        statement = select(Person).where(Person.id == risid)
-        return session.exec(statement).first()
+    statement = select(Person).where(Person.id == risid)
+    person = _session.exec(statement).first()
+    return person
 
 
 def save_object_to_database(object):
-    with Session(_engine) as session:
-        session.add(object)
-        session.commit()
+    _session.add(object)
+    _session.commit()
 
 
 def save_objects_to_database(objects: list[object]):
     for object in objects:
-        save_object_to_database(object)
+        _session.add(object)
+    _session.commit()
