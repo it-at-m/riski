621de435e40fc27d9f450b18c4febc148ed31d3a
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-09-18 08:46:43 +0200

    test: add unit test

---
+++ riski-extractor/test/test_db_access.py
@@ -0,0 +1,15 @@
+from src.data_models import Person
+from src.db_access import request_person_by_risid
+
+
+# ----------------------
+# Test Person
+# ----------------------
+def test_request_person_by_risid(person: Person):
+    db_person = request_person_by_risid(person.id)
+    assert db_person.db_id == person.db_id
+    assert db_person.id == person.id
+    assert db_person.name == person.name
+    assert db_person.title == person.title
+    assert db_person.created == person.created
+    assert db_person.modified == person.modified
