4de43cc4c2319c1276c51efd0c370f973ae68ef2
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-09-29 16:42:11 +0200

    fix: fix test, code review

--- riski-extractor/test/test_db_access.py
+++ riski-extractor/test/test_db_access.py
@@ -1,11 +1,12 @@
-from src.db_access import request_person_by_risid
+from src.data_models import Person
+from src.db.db_access import request_object_by_risid
 
 
 # ----------------------
 # Test Person
 # ----------------------
 def test_request_person_by_risid(session, person):
-    db_person = request_person_by_risid(risid=person.id, session=session)
+    db_person = request_object_by_risid(risid=person.id, t=Person, session=session)
     assert db_person.db_id == person.db_id
     assert db_person.id == person.id
     assert db_person.name == person.name
