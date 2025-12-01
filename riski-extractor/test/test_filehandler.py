d41f39ec60e49a5b665343cf399b70744194267c
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-12-01 14:01:22 +0100

    refactor:  code rabbit

--- riski-extractor/test/test_filehandler.py
+++ riski-extractor/test/test_filehandler.py
@@ -48,7 +48,7 @@ def test_download_and_persist_file_updates_filename_urlencoding(filehandler_inst
         assert mock_file.size == len(b"test content")
 
 
-def test_download_and_persist_file_not_updates_filename_unchanged_file(filehandler_instance, mock_file):
+def test_download_and_persist_file_not_updates_filename_when_unchanged_file(filehandler_instance, mock_file):
     mock_response = MagicMock()
     mock_response.content = b"test"
     mock_response.headers = {"content-disposition": 'inline; filename="test_file.txt"'}
@@ -57,9 +57,10 @@ def test_download_and_persist_file_not_updates_filename_unchanged_file(filehandl
     mock_file.content = b"test"
     mock_file.size = len(b"test")
 
-    with patch("src.db.db_access.update_or_insert_objects_to_database"):
+    with patch("src.db.db_access.update_or_insert_objects_to_database") as mock_update:
         filehandler_instance.download_and_persist_file(mock_file)
 
         assert mock_file.fileName == "initial_filename.pdf"
         assert mock_file.content == b"test"
         assert mock_file.size == len(b"test")
+        mock_update.assert_not_called()
