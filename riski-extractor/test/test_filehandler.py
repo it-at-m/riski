000cac4764f5197a5401171b368b3b8f140e3d63
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-12-01 12:46:24 +0100

    test: add test for url encoding

--- riski-extractor/test/test_filehandler.py
+++ riski-extractor/test/test_filehandler.py
@@ -33,6 +33,21 @@ def test_download_and_persist_file_updates_filename(filehandler_instance, mock_f
         assert mock_file.size == len(b"test content")
 
 
+def test_download_and_persist_file_updates_filename_urlencoding(filehandler_instance, mock_file):
+    mock_response = MagicMock()
+    mock_response.content = b"test content"
+    mock_response.headers = {"content-disposition": 'inline; filename="test%20file.txt"'}
+
+    filehandler_instance.client.get = MagicMock(return_value=mock_response)
+
+    with patch("src.db.db_access.update_or_insert_objects_to_database"):
+        filehandler_instance.download_and_persist_file(mock_file)
+
+        assert mock_file.fileName == "test file.txt"
+        assert mock_file.content == b"test content"
+        assert mock_file.size == len(b"test content")
+
+
 def test_download_and_persist_file_not_updates_filename_unchanged_file(filehandler_instance, mock_file):
     mock_response = MagicMock()
     mock_response.content = b"test"
