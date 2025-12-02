9840884cd293f00ab605faa15b063d81f1122987
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2025-12-02 09:06:11 +0100

    fix: unit tests

--- riski-extractor/test/test_confidential_file_deleter.py
+++ riski-extractor/test/test_confidential_file_deleter.py
@@ -1,9 +1,9 @@
 import datetime
-from unittest.mock import patch
+from unittest.mock import call, patch
 
 import pytest
 from src.data_models import File
-from src.filehandler.confidential_file_deleter import ConfidentialFileDeleter  # Replace `your_module` with the actual module name
+from src.filehandler.confidential_file_deleter import ConfidentialFileDeleter
 
 
 @pytest.fixture
@@ -16,36 +16,30 @@ def deleter():
 @patch("src.filehandler.confidential_file_deleter.request_all")
 @patch("src.filehandler.confidential_file_deleter.remove_object_by_id")
 def test_delete_confidential_files(mock_remove_object_by_id, mock_request_all, mock_get_all_found_file_ids, deleter):
-    # Arrange
     mock_get_all_found_file_ids.return_value = ["file1", "file2"]
     mock_remove_object_by_id.return_value = None
 
-    # Simulate the request_all return value
     mock_request_all.return_value = [
         File(id="file1", modified=datetime.datetime(2023, 10, 1)),
         File(id="file2", modified=datetime.datetime(2023, 10, 2)),
-        File(id="file3", modified=datetime.datetime(2023, 10, 3)),  # This one should be deleted
-        File(id="file4", modified=datetime.datetime(2023, 9, 30)),  # This one should not be deleted
+        File(id="file3", modified=datetime.datetime(2023, 10, 3)),
+        File(id="file4", modified=datetime.datetime(2023, 10, 30)),
     ]
 
-    # Mock config to have a start_date
     deleter.config.start_date = "2023-10-01"
 
-    # Act
     deleter.delete_confidential_files()
 
-    # Assert
-    mock_remove_object_by_id.assert_called_once_with("file3", File)
+    calls = [call("file3", File), call("file4", File)]
+    mock_remove_object_by_id.assert_has_calls(calls)
 
 
 @patch("src.filehandler.confidential_file_deleter.get_all_found_file_ids")
 @patch("src.filehandler.confidential_file_deleter.request_all")
 @patch("src.filehandler.confidential_file_deleter.remove_object_by_id")
 def test_no_files_deleted(mock_remove_object_by_id, mock_request_all, mock_get_all_found_file_ids, deleter):
-    # Arrange
     mock_get_all_found_file_ids.return_value = ["file1", "file2"]
 
-    # Simulate the request_all return value
     mock_request_all.return_value = [
         File(id="file1", modified=datetime.datetime(2023, 10, 1)),
         File(id="file2", modified=datetime.datetime(2023, 10, 2)),
@@ -53,34 +47,26 @@ def test_no_files_deleted(mock_remove_object_by_id, mock_request_all, mock_get_a
 
     mock_remove_object_by_id.return_value = None
 
-    # Mock config to have a start_date
     deleter.config.start_date = "2023-10-01"
 
-    # Act
     deleter.delete_confidential_files()
 
-    # Assert
-    mock_remove_object_by_id.assert_not_called()  # No files should be deleted
+    mock_remove_object_by_id.assert_not_called()
 
 
 @patch("src.filehandler.confidential_file_deleter.get_all_found_file_ids")
 @patch("src.filehandler.confidential_file_deleter.request_all")
 @patch("src.filehandler.confidential_file_deleter.remove_object_by_id")
 def test_files_not_modified_after_start_date(mock_remove_object_by_id, mock_request_all, mock_get_all_found_file_ids, deleter):
-    # Arrange
     mock_get_all_found_file_ids.return_value = []
 
-    # Simulate the request_all return value
     mock_request_all.return_value = [
-        File(id="file1", modified=datetime.datetime(2023, 9, 30)),  # This should not be deleted
-        File(id="file2", modified=datetime.datetime(2023, 9, 29)),  # This should not be deleted
+        File(id="file1", modified=datetime.datetime(2023, 9, 30)),
+        File(id="file2", modified=datetime.datetime(2023, 9, 29)),
     ]
 
-    # Mock config to have a start_date
     deleter.config.start_date = "2023-10-01"
 
-    # Act
     deleter.delete_confidential_files()
 
-    # Assert
     mock_remove_object_by_id.assert_not_called()
