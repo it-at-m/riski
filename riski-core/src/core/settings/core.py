03f242439e7fa5463ed4c8d2ac7afe0ec2b22c88
Stefan Hentschel <stefan.hentschel@muenchen.de>
2025-12-09 15:23:44 +0100

    chore: move genereal settings to riski-core

---
+++ riski-core/src/core/settings/core.py
@@ -0,0 +1,15 @@
+from core.settings.db import DatabaseSettings
+from core.settings.lm import LanguageModelSettings
+from pydantic import BaseModel, Field
+
+
+class CoreSettings(BaseModel):
+    db: DatabaseSettings = Field(
+        default_factory=lambda: DatabaseSettings(),
+        description="Postgres related settings",
+    )
+
+    lm: LanguageModelSettings = Field(
+        default_factory=lambda: LanguageModelSettings(),
+        description="Language model related settings",
+    )
