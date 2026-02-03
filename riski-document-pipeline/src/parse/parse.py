7856b780d63841d21bf07662638bcd9e2b16b3f2
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2026-02-03 09:19:33 +0100

    feat: batching in document pipeline parser

--- riski-document-pipeline/src/parse/parse.py
+++ riski-document-pipeline/src/parse/parse.py
@@ -1,11 +1,10 @@
 import base64
 from io import BytesIO
 
-from core.db.db_access import _get_session_ctx
+from core.db.db_access import _get_session_ctx, request_batch
 from core.model.data_models import File
 from mistralai import Mistral
 from pypdf import PdfReader, PdfWriter
-from sqlmodel import select
 
 from src.logtools import getLogger
 
@@ -19,47 +18,58 @@ def run_ocr_for_documents(settings):
     client = Mistral(api_key=api_key, server_url=server_url)
     ocr_model = settings.ocr_model_name
     max_docs = settings.max_documents_to_process
+    batch_size = settings.max_documents_to_process
+
     if max_docs is not None and max_docs <= 0:
         logger.info("max_documents_to_process is %s; skipping OCR run.", max_docs)
         return
 
     with _get_session_ctx() as session:
-        # Query documents which haven't had OCR yet TODO: replace with kafka consumer
-        docs = session.exec(select(File).where(File.content != None, File.text == None)).all()  # noqa: E711
-        logger.info(f"Found {len(docs)} documents to process.")
-
-        docs_to_process = docs if max_docs is None else docs[:max_docs]
+        offset = 0
         if max_docs is not None:
             logger.info(
-                "Processing up to %s documents (actual: %s)",
+                "Processing up to %s documents",
                 max_docs,
-                len(docs_to_process),
             )
+        while True:
+            if batch_size + offset > max_docs:
+                limit = max_docs
+            else:
+                limit = batch_size
+            docs_to_process: list[File] = request_batch(File, offset=offset, limit=limit)
 
-        for doc in docs_to_process:
-            logger.debug(f"Processing doc id={doc.id}")
-            pages_text = []
-            for pdf_chunk in chunk_pdf_into_max_page_blocks(doc.content, chunk_size=30):
-                base64_pdf = base64.b64encode(pdf_chunk).decode("utf-8")
-                try:
-                    resp = client.ocr.process(
-                        model=ocr_model,
-                        document={"type": "document_url", "document_url": f"data:application/pdf;base64,{base64_pdf}"},
-                    )
-                    chunk_pages_text = [page.markdown for page in resp.pages]
-                    logger.debug(chunk_pages_text[: min(3, len(chunk_pages_text))])
+            if not docs_to_process:
+                break
 
-                    pages_text.extend(chunk_pages_text)
-                except Exception as e:
-                    logger.error(f"Error processing OCR for doc id={doc.id}: {e}")
-                    continue
+            for doc in docs_to_process:
+                logger.debug(f"Processing doc id={doc.id}")
+                pages_text = []
+                for pdf_chunk in chunk_pdf_into_max_page_blocks(doc.content, chunk_size=30):
+                    base64_pdf = base64.b64encode(pdf_chunk).decode("utf-8")
+                    try:
+                        resp = client.ocr.process(
+                            model=ocr_model,
+                            document={"type": "document_url", "document_url": f"data:application/pdf;base64,{base64_pdf}"},
+                        )
+                        chunk_pages_text = [page.markdown for page in resp.pages]
+                        logger.debug(chunk_pages_text[: min(3, len(chunk_pages_text))])
 
-            # Combine all pages' markdown into one text blob
-            full_markdown = "\n\n".join(pages_text)
-            # Save to db object
-            doc.text = full_markdown
-            session.add(doc)
-            session.commit()
+                        pages_text.extend(chunk_pages_text)
+                    except Exception as e:
+                        logger.error(f"Error processing OCR for doc id={doc.id}: {e}")
+                        continue
+
+                # Combine all pages' markdown into one text blob
+                full_markdown = "\n\n".join(pages_text)
+                # Save to db object
+                doc.text = full_markdown
+                session.add(doc)
+                session.commit()
+
+            if offset + batch_size >= max_docs:
+                break
+
+            offset += batch_size
 
 
 def chunk_pdf_into_max_page_blocks(pdf_bytes: bytes, chunk_size: int = 30) -> list[bytes]:
