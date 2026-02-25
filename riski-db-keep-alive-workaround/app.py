20bf77d3f4dae59e1e3b22a43bf2a21ac474a5fc
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2026-02-25 15:08:26 +0100

    no cookies

--- riski-db-keep-alive-workaround/app.py
+++ riski-db-keep-alive-workaround/app.py
@@ -31,17 +31,8 @@ def main():
             "messages": [{"id": str(uuid.uuid4()), "role": "user", "content": "Welche Anträge gibt es zum Thema Radverkehr?"}],
         }
 
-        cookies = {
-            "ajs_user_id": "8fc0542151148372d0174415a66f10cff5a1f5c0",
-            "ajs_anonymous_id": "8c2f864b-79e5-4eb7-ae23-814c50cfd3ae",
-            "analytics_session_id": "1771951287072",
-            "analytics_session_id.last_access": "1771951287072",
-            "41e917a471a3f7ad9547afd27875fbbc": "fc30cf981a2e1f29e5251ba19f40cbc8",
-            "BIGSC": "!mg5TwJqGFPLYzYiw6q1zOJj8NrwSwMdldCwO5fWd8RWoFT65acqtiIXdaa5FMnmbBCECYC/oLroju5o=",
-        }
-
         logger.info(f"Request {config.request_url}")
-        response = client.post(url=config.request_url, json=payload, cookies=cookies)
+        response = client.post(url=config.request_url, json=payload)
         logger.info(f"Request Finished {response.status_code}")
         time.sleep(config.request_interval)
 
